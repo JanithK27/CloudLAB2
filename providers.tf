38cf3b45e70f5d39c6326f82a0e96757ab7d3235
Janith Hettiarachchi <hettiarachchijanithkavinda04@gmail.com>
2024-01-04 20:23:05 +0530

    ee

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "janithlab2-tfstate-bkt" #change this  
+    
     key    = "terraform.tfstate"
     region = "us-east-1"
     #dynamodb_table = "my-lock-table" # optional (Only if you created the DynamoDB table in step 4) 
